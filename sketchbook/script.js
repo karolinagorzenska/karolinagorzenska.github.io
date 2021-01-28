707ebacd5241320bcb15e4130bee8fcd85959cab
mbkorecki <mbokorecki@gmail.com>
2021-01-28 12:48:54 +0100

    .

---
+++ sketchbook/script.js
@@ -0,0 +1,47 @@
+var picture = document.querySelector('picture');
+
+function addImages(amount)
+{
+    for (var i = 0; i <= (amount); ++i)
+    {
+        var image = document.createElement("img");
+        image.setAttribute("src", "../images/sketchbook/" + i + ".png");
+        image.setAttribute("id", i);
+        image.setAttribute("class", "column");
+        image.setAttribute("onclick", "openGallery(this)");
+        document.querySelector("picture").appendChild(image);
+    }
+}
+
+function next(amount)
+{
+    var slide = document.getElementsByClassName("slide")[0];
+    var idx = Number(slide.getAttribute('id')) + 1;
+    if (idx > amount)
+        idx = 0;
+    slide.src = "../images/sketchbook/" + idx + ".jpg";
+    slide.id = idx;
+}
+
+function prev(amount)
+{
+    var slide = document.getElementsByClassName("slide")[0];
+    var idx = Number(slide.getAttribute('id')) - 1;
+    if (idx < 0)
+        idx = amount;
+    slide.src = "../images/sketchbook/" + idx + ".jpg";
+    slide.id = idx;
+}
+
+function openGallery(source)
+{
+    document.getElementById('gallery').style.display = "grid";
+    var slide = document.getElementsByClassName("slide")[0];
+    slide.id = source.getAttribute('id');
+    slide.src = "../images/sketchbook/" + slide.id + ".jpg";
+}
+
+function closeGallery()
+{
+    document.getElementById('gallery').style.display = "none";
+}
